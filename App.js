4707c81ab89afcd8d90b383d3dfe819aabf043ae
Olsi <olsi.hoxha.08@protonmail.ch>
2020-10-20 20:29:48 +0200

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Asset} from 'expo-asset';
+import {AppLoading} from 'expo';
+import MainBody from './app/index';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+export default class App extends React.Component {
+  
+constructor(){
+  super();
+  this.state={
+    isReady: false,
+  }
+          }
+
+  async _loadAssetsAsync() {
+        const imageAssets = cacheImages([
+              require('./assets/logiapp.png'),
+            ]);
+         await Promise.all([...imageAssets]);
+          }
+
+  render(){
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return (
+    <MainBody/>
   );
+}
 }
 
 const styles = StyleSheet.create({
---
+++ app/index.js
@@ -0,0 +1,217 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Dimensions,TextInput} from 'react-native';
+import Animated,{Easing} from 'react-native-reanimated';
+import {TapGestureHandler,State} from 'react-native-gesture-handler';
+import Svg,{Image,Circle,ClipPath} from 'react-native-svg';
+
+
+const {Value,
+    event,
+    block,
+    cond,
+    eq,
+    set,
+    Clock,
+    startClock,
+    stopClock,
+    debug,
+    timing,
+    clockRunning,
+    interpolate,
+    Extrapolate,
+    concat
+        }=Animated;
+const {width,height}=Dimensions.get('window');
+///////
+function runTiming(clock, value, dest) {
+    const state = {
+      finished: new Value(0),
+      position: new Value(0),
+      time: new Value(0),
+      frameTime: new Value(0)
+    };
+  
+    const config = {
+      duration: 1000,
+      toValue: new Value(0),
+      easing: Easing.inOut(Easing.ease)
+    };
+  
+    return block([
+      cond(clockRunning(clock), 0, [
+        set(state.finished, 0),
+        set(state.time, 0),
+        set(state.position, value),
+        set(state.frameTime, 0),
+        set(config.toValue, dest),
+        startClock(clock)
+      ]),
+      timing(clock, state, config),
+      cond(state.finished, debug('stop clock', stopClock(clock))),
+      state.position
+    ]);
+  }
+  ////////
+export default class MainBody extends React.Component{
+    constructor(){
+        super();
+        this.buttonOpacity=new Value(1);
+
+        this.onStateChange=event([{
+            nativeEvent:({state})=>block([
+                cond(eq(state,State.END),set(this.buttonOpacity,runTiming(new Clock(),1,0)))
+            ])
+        }]);
+
+
+        this.onCloseState=event([{
+            nativeEvent:({state})=>block([
+                cond(eq(state,State.END),set(this.buttonOpacity,runTiming(new Clock(),0,1)))
+            ])
+        }]);
+
+        this.buttonY=interpolate(this.buttonOpacity,{
+            inputRange:[0,1],
+            outputRange:[100,0],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.bgY=interpolate(this.buttonOpacity,{
+            inputRange:[0,1],
+            outputRange:[-height/3-50,0],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.textInputZIndex=interpolate(this.buttonOpacity,{
+            inputRange:[0,1],
+            outputRange:[1,-1],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.textInputY=interpolate(this.buttonOpacity,{
+            inputRange:[0,1],
+            outputRange:[0,100],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.textInputOpacity=interpolate(this.buttonOpacity,{
+            inputRange:[0,1],
+            outputRange:[1,0],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.rotateCross=interpolate(this.buttonOpacity,{
+            inputRange:[0,1],
+            outputRange:[180,360],
+            extrapolate:Extrapolate.CLAMP
+        });
+    }
+    render(){
+        return(
+            <View style={{flex:1,backgroundColor:'white',justifyContent:'flex-end'}}> 
+            <Animated.View style={{...StyleSheet.absoluteFill,transform:[{translateY:this.bgY}]}}>
+            <Svg height={height+50} width={width}>
+                <ClipPath id='clip'>
+                <Circle r={height+50} cx={width/2}/>
+                </ClipPath>
+            <Image 
+            source={require('../assets/logiapp.png')}
+            href={require('../assets/logiapp.png')}
+            width={width}
+            height={height+50}
+            preserveAspectRatio='xMidYMid slice'
+            clipPath='url(#clip)'
+            />
+            </Svg>
+            </Animated.View>
+            <View style={{height:height / 3,justifyContent:'center'}}>
+                <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+            <Animated.View style={{...styles.buttons,opacity:this.buttonOpacity,
+            transform:[{translateY:this.buttonY}]
+            }}>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>Sign In</Text>
+            </Animated.View>
+                </TapGestureHandler>
+                
+            <Animated.View style={{...styles.buttons,backgroundColor:'#2E71DC',opacity:this.buttonOpacity,
+            transform:[{translateY:this.buttonY}]}}>
+            <Text style={{fontSize:20,fontWeight:'bold',color:'white'}}>Sign In With Facebook</Text>
+            </Animated.View>
+            <Animated.View style={{height:height/3,
+                ...StyleSheet.absoluteFill,
+                top:null,
+                justifyContent:'center',
+                zIndex:this.textInputZIndex,
+                opacity:this.textInputOpacity,
+                transform:[{translateY:this.textInputY}]
+                }}>
+            <TapGestureHandler onHandlerStateChange={this.onCloseState}>
+            <Animated.View style={styles.closeButton}>
+                <Animated.Text style={{fontSize:15,
+                transform:[{rotate:concat(this.rotateCross,'deg')}]
+                }}>X</Animated.Text>
+            </Animated.View>
+            </TapGestureHandler>
+            <TextInput 
+            placeholder='Email'
+            style={styles.textInput}
+            placeholderTextColor='black'
+            />
+             <TextInput 
+            placeholder='Password'
+            style={styles.textInput}
+            placeholderTextColor='black'
+            secureTextEntry
+            />
+            <Animated.View style={styles.buttons}>
+            <Text style={{fontSize:20,fontWeight:'bold'}}>Sign In</Text>
+            </Animated.View>
+            </Animated.View>
+            </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    buttons: {
+      backgroundColor: 'white',
+      height: 60,
+      marginHorizontal:20,
+      borderRadius:35,
+      alignItems:'center',
+      justifyContent:'center',
+      marginVertical:5,
+      elevation: 3,
+      shadowOffset:{width:2,height:2},
+      shadowColor:'black',
+      shadowOpacity:0.2,
+    },
+    textInput:{
+        height:50,
+        borderRadius:25,
+        borderWidth:0.5,
+        marginHorizontal:20,
+        paddingLeft:10,
+        marginVertical:5,
+        borderColor:'rgba(0,0,0,0.2)'
+    },
+    closeButton:{
+        height:40,
+        width:40,
+        borderRadius:20,
+        position:'absolute',
+        top:-28,
+        left:width/2 - 20,
+        backgroundColor:'white',
+        alignItems:'center',
+        justifyContent:'center',
+        elevation: 2,
+        shadowOffset:{width:2,height:2},
+        shadowColor:'black',
+        shadowOpacity:0.2,
+        
+    }
+  });
+  
